8c9a64c70c1f4baf75d6b4dcd1b5da686f741dd3
suraj savle <surajsavle143@gmail.com>
2025-09-19 17:51:03 +0530

    feat: Enhance student profile component with API integration for fetching profile data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
 import Landing from "./pages/Landing";
 import  RegisterPage  from "./pages/auth/Register";
-import  LoginPage  from "./pages/auth/Login";
+import LoginPage from "./pages/auth/Login";
+// import Apply from "./pages/student/Apply";
+// import UploadDocs from "./pages/student/UploadDocs";
+// import Status from "./pages/student/Status";
+// import Transactions from "./pages/student/Transactions";
+import Profile from "./pages/student/profile";
+import DashboardLayout from "./pages/student/Dashboard";
+
 
 
 
@@ -15,14 +22,14 @@ function App() {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
 
-      {/* <Route path="/student" element={<DashboardLayout />}> */}
+      <Route path="/student" element={<DashboardLayout />}>
         {/* <Route index element={<StudentDashboardHome />} /> */}
         {/* <Route path="apply" element={<Apply />} /> */}
         {/* <Route path="upload" element={<UploadDocs />} /> */}
         {/* <Route path="status" element={<Status />} /> */}
         {/* <Route path="transactions" element={<Transactions />} /> */}
-        {/* <Route path="profile" element={<Profile />} /> */}
-      {/* </Route> */}
+        <Route path="profile" element={<Profile />} />
+      </Route>
 
      
     </Routes>
